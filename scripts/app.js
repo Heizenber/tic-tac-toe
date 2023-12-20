7c3759734739fe76b5a2192494b7bf8f0b5517a2
Heizenberg <popov.roman91@gmail.com>
2023-12-20 10:42:47 +0200

    added possibility to turn on and off the overlay

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,15 @@
-const playerConfigOverlay = document.getElementById('player-config-overlay');
+const playerConfigOverlay = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 
-const editPlayer1Btn = document.getElementById('edit-player1-btn');
-const editPlayer2Btn = document.getElementById('edit-player2-btn');
+const editPlayer1Btn = document.getElementById('edit-player-1-btn');
+const editPlayer2Btn = document.getElementById('edit-player-2-btn');
+const cancelConfigBtn = document.getElementById('cancel-config-btn');
+
 
 
 
 editPlayer1Btn.addEventListener('click', openPlayerConfig) 
 editPlayer2Btn.addEventListener('click', openPlayerConfig) 
+
+cancelConfigBtn.addEventListener('click', closePlayerConfig)
+backdropElement.addEventListener('click', closePlayerConfig)
