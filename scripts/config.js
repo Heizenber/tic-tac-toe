f59c81db9914f556a57a77038df76ea8f27f6e1c
Heizenberg <popov.roman91@gmail.com>
2023-12-20 11:39:06 +0200

    finished with reading input from the users

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,5 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+    editedPlayer = Number(event.target.dataset.playerid);
     playerConfigOverlay.style.display = 'block';
     backdropElement.style.display = 'block';
 }
@@ -8,6 +9,7 @@ function closePlayerConfig() {
     backdropElement.style.display = 'none';
     formElement.firstElementChild.classList.remove('error');
     errorsOutput.textContent = '';
+    formElement.firstElementChild.lastElementChild.value = '';
 }
 
 function savePlayerConfig(event) {
@@ -19,4 +21,11 @@ function savePlayerConfig(event) {
         errorsOutput.textContent = 'Please enter a valid name!';
         return
     }
+    const updatedPlayerDataElement = document.getElementById(`player-${editedPlayer}-data`);
+    updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+
+    players[editedPlayer - 1].name = enteredPlayerName;
+
+    closePlayerConfig();
+    
 }
