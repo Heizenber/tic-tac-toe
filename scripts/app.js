d0e8b4f0d77481d3236435b1051759d594550fb7
Heizenberg <popov.roman91@gmail.com>
2023-12-20 10:15:49 +0200

    added basic javascript files

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+const playerConfigOverlay = document.getElementById('player-config-overlay');
+const backdropElement = document.getElementById('backdrop');
+
+const editPlayer1Btn = document.getElementById('edit-player1-btn');
+const editPlayer2Btn = document.getElementById('edit-player2-btn');
+
+
+
+editPlayer1Btn.addEventListener('click', openPlayerConfig) 
+editPlayer2Btn.addEventListener('click', openPlayerConfig) 
